b43e8ba42e7c65f488901084cef8aaa419ec13df
Egor Ovchinnikov <desk10567@gmail.com>
2023-04-14 15:33:53 +0300

    feat: implemented functionality for working with configs

---
+++ src/environment/environment.go
@@ -0,0 +1,37 @@
+package environment
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+var owner string
+var repository string
+var path string
+
+func SetUpEnv() {
+	viper.SetConfigFile("./../configs/app.env")
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println(err)
+	}
+	owner = viper.Get("owner").(string)
+	repository = viper.Get("repository").(string)
+	path = viper.Get("path").(string)
+}
+
+// Returns the name of owner of the repository with addons
+func GetOwner() string {
+	return owner
+}
+
+// Returns the name of the repository with addons
+func GetRepository() string {
+	return repository
+}
+
+// Returns the path to the addons in the repository
+func GetPath() string {
+	return path
+}
