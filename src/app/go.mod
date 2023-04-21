fb066c22f5c9930603b747159467016bef3164bb
Egor Ovchinnikov <desk10567@gmail.com>
2023-04-21 18:01:58 +0300

    feat: implemented the display of addons using fake data

--- src/app/go.mod
+++ src/app/go.mod
@@ -3,14 +3,15 @@ module app
 go 1.20
 
 require (
-	github.com/charmbracelet/bubbles v0.15.0
 	github.com/charmbracelet/bubbletea v0.23.2
-	github.com/charmbracelet/lipgloss v0.7.1
+	github.com/evertras/bubble-table v0.15.2
 )
 
 require (
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/bubbles v0.11.0 // indirect
+	github.com/charmbracelet/lipgloss v0.5.0 // indirect
 	github.com/containerd/console v1.0.3 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
@@ -21,7 +22,6 @@ require (
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/sahilm/fuzzy v0.1.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.6.0 // indirect
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
--- src/app/main.go
+++ src/app/main.go
@@ -3,76 +3,44 @@ package main
 import (
 	"fmt"
 	"os"
+	"requests"
 
 	env "environment"
-	"github.com/charmbracelet/bubbles/list"
+
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
-	"requests"
+	"github.com/evertras/bubble-table/table"
+	"view"
 )
-
-var columnStyle = lipgloss.NewStyle().Margin(1, 2)
-
-type Item struct {
-	title string
-	desc  string
-}
-
-func (i Item) Title() string       { return i.title }
-func (i Item) Description() string { return i.desc }
-func (i Item) FilterValue() string { return i.title }
-
-type model struct {
-	list list.Model
-}
-
-func InitModel() (*model, error) {
-	response, err := requests.GetListAddons()
-	if err != nil {
-		return nil, err
-	}
-	items := []list.Item{}
-	for _, v := range response.Value() {
-		items = append(items, Item{title: v.Name, desc: v.Path})
-	}
-	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
-	m.list.Title = "List addons"
-	return &m, nil
-}
-
-func (m model) Init() tea.Cmd {
-	return nil
-}
-
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg := msg.(type) {
-	case tea.KeyMsg:
-		if msg.String() == "ctrl+c" {
-			return m, tea.Quit
-		}
-	case tea.WindowSizeMsg:
-		h, v := columnStyle.GetFrameSize()
-		m.list.SetSize(msg.Width-h, msg.Height-v)
-	}
-	var cmd tea.Cmd
-	m.list, cmd = m.list.Update(msg)
-	return m, cmd
-}
-
-func (m model) View() string {
-	return columnStyle.Render(m.list.View())
-}
 
 func main() {
 	env.SetUpEnv()
-	m, err := InitModel()
+	list, err := requests.GetListAddons()
 	if err != nil {
-		fmt.Println("Runtime error:", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
-	p := tea.NewProgram(*m, tea.WithAltScreen())
+
+	items := view.NewItems([]table.Row{})
+	for i, v := range list.Value() {
+		if i%2 == 0 {
+			items.Append(view.Item{
+				Image:          fmt.Sprintf("%s%v", "Image", i),
+				Title:          fmt.Sprintf("%s%v", v.Name, i),
+				Status:         "installed",
+				CurrentVersion: "1.1.0",
+				LastVersion:    "1.1.0"})
+		} else {
+			items.Append(view.Item{
+				Image:          fmt.Sprintf("%s%v", "Image", i),
+				Title:          fmt.Sprintf("%s%v", v.Name, i),
+				Status:         "not installed",
+				CurrentVersion: "0.0.8",
+				LastVersion:    "0.0.9"})
+		}
+	}
+	p := tea.NewProgram(view.NewModel(items.GetRows()), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
-		fmt.Println("Runtime error:", err)
+		fmt.Println(err)
 		os.Exit(1)
 	}
 }
