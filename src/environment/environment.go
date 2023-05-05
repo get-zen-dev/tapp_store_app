65df71fcb9fb68b3342f873471ae5d45b40abdab
RazerFord <desk10567@gmail.com>
2023-05-05 11:57:44 +0300

    fix: name of vars

--- src/environment/environment.go
+++ src/environment/environment.go
@@ -6,10 +6,12 @@ import (
 	"github.com/spf13/viper"
 )
 
-var owner string
-var repository string
-var path string
-var ref string
+var (
+	owner      string
+	repository string
+	path       string
+	ref        string
+)
 
 func SetUpEnv() {
 	viper.SetConfigFile("./../configs/app.env")
