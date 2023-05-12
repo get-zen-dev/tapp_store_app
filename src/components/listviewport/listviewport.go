e6513395b57ad55f849356f80ff2afdda358b300
RazerFord <desk10567@gmail.com>
2023-05-12 14:48:15 +0300

    feat: port new table

---
+++ src/components/listviewport/listviewport.go
@@ -0,0 +1,154 @@
+package listviewport
+
+import (
+	"fmt"
+	"style"
+	"time"
+
+	"common"
+	"constants"
+	"github.com/charmbracelet/bubbles/viewport"
+	"github.com/charmbracelet/lipgloss"
+)
+
+type Model struct {
+	style           style.Styles
+	viewport        viewport.Model
+	topBoundId      int
+	bottomBoundId   int
+	currId          int
+	ListItemHeight  int
+	NumCurrentItems int
+	NumTotalItems   int
+	LastUpdated     time.Time
+	ItemTypeLabel   string
+}
+
+func NewModel(style style.Styles, dimensions constants.Dimensions, lastUpdated time.Time, itemTypeLabel string, numItems, listItemHeight int) Model {
+	model := Model{
+		NumCurrentItems: numItems,
+		ListItemHeight:  listItemHeight,
+		currId:          0,
+		viewport: viewport.Model{
+			Width:  dimensions.Width,
+			Height: dimensions.Height - common.ListPagerHeight,
+		},
+		topBoundId:    0,
+		ItemTypeLabel: itemTypeLabel,
+		LastUpdated:   lastUpdated,
+	}
+	model.bottomBoundId = Min(model.NumCurrentItems-1, model.getNumPrsPerPage()-1)
+	return model
+}
+
+func (m *Model) SetNumItems(numItems int) {
+	m.NumCurrentItems = numItems
+	m.bottomBoundId = Min(m.NumCurrentItems-1, m.getNumPrsPerPage()-1)
+}
+
+func (m *Model) SetTotalItems(total int) {
+	m.NumTotalItems = total
+}
+
+func (m *Model) SyncViewPort(content string) {
+	m.viewport.SetContent(content)
+}
+
+func (m *Model) getNumPrsPerPage() int {
+	return m.viewport.Height / m.ListItemHeight
+}
+
+func (m *Model) ResetCurrItem() {
+	m.currId = 0
+	m.viewport.GotoTop()
+}
+
+func (m *Model) GetCurrItem() int {
+	return m.currId
+}
+
+func (m *Model) NextItem() int {
+	atBottomOfViewport := m.currId >= m.bottomBoundId
+	if atBottomOfViewport {
+		m.topBoundId += 1
+		m.bottomBoundId += 1
+		m.viewport.LineDown(m.ListItemHeight)
+	}
+
+	newId := Min(m.currId+1, m.NumCurrentItems-1)
+	newId = Max(newId, 0)
+	m.currId = newId
+	return m.currId
+}
+
+func (m *Model) PrevItem() int {
+	atTopOfViewport := m.currId < m.topBoundId
+	if atTopOfViewport {
+		m.topBoundId -= 1
+		m.bottomBoundId -= 1
+		m.viewport.LineUp(m.ListItemHeight)
+	}
+
+	m.currId = Max(m.currId-1, 0)
+	return m.currId
+}
+
+func (m *Model) FirstItem() int {
+	m.currId = 0
+	m.viewport.GotoTop()
+	return m.currId
+}
+
+func (m *Model) LastItem() int {
+	m.currId = m.NumCurrentItems - 1
+	m.viewport.GotoBottom()
+	return m.currId
+}
+
+func (m *Model) SetDimensions(dimensions constants.Dimensions) {
+	m.viewport.Height = dimensions.Height - common.ListPagerHeight
+	m.viewport.Width = dimensions.Width
+}
+
+func (m *Model) View() string {
+	pagerContent := ""
+	if m.NumTotalItems > 0 {
+		pagerContent = fmt.Sprintf(
+			"%v %v • %v %v/%v • Fetched %v",
+			constants.WaitingIcon,
+			m.LastUpdated.Format("01/02 15:04:05"),
+			m.ItemTypeLabel,
+			m.currId+1,
+			m.NumTotalItems,
+			m.NumCurrentItems,
+		)
+	}
+	viewport := m.viewport.View()
+	pager := m.style.ListViewPort.PagerStyle.Copy().Render(pagerContent)
+	return lipgloss.NewStyle().
+		Width(m.viewport.Width).
+		MaxWidth(m.viewport.Width).
+		Render(lipgloss.JoinVertical(
+			lipgloss.Left,
+			viewport,
+			pager,
+		))
+}
+
+func (m *Model) UpdateStyle(style *style.Styles) {
+	m.style = *style
+}
+
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
---
+++ src/components/style/style.go
@@ -0,0 +1,74 @@
+package style
+
+import (
+	"common"
+	bbHelp "github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/lipgloss"
+	"theme"
+)
+
+type Styles struct {
+	Common common.CommonStyles
+
+	Help struct {
+		Text         lipgloss.Style
+		KeyText      lipgloss.Style
+		BubbleStyles bbHelp.Styles
+	}
+	ListViewPort struct {
+		PagerHeight int
+		PagerStyle  lipgloss.Style
+	}
+	Table struct {
+		CellStyle                lipgloss.Style
+		SelectedCellStyle        lipgloss.Style
+		TitleCellStyle           lipgloss.Style
+		SingleRuneTitleCellStyle lipgloss.Style
+		HeaderStyle              lipgloss.Style
+		RowStyle                 lipgloss.Style
+	}
+}
+
+func InitStyles(theme theme.Theme) Styles {
+	var s Styles
+
+	s.Common = common.BuildStyles(theme)
+
+	s.Help.Text = lipgloss.NewStyle().Foreground(theme.SecondaryText)
+	s.Help.KeyText = lipgloss.NewStyle().Foreground(theme.PrimaryText)
+	s.Help.BubbleStyles = bbHelp.Styles{
+		ShortDesc:      s.Help.Text.Copy().Foreground(theme.FaintText),
+		FullDesc:       s.Help.Text.Copy().Foreground(theme.FaintText),
+		ShortSeparator: s.Help.Text.Copy().Foreground(theme.SecondaryBorder),
+		FullSeparator:  s.Help.Text.Copy(),
+		FullKey:        s.Help.KeyText.Copy(),
+		ShortKey:       s.Help.KeyText.Copy(),
+		Ellipsis:       s.Help.Text.Copy(),
+	}
+
+	s.ListViewPort.PagerStyle = lipgloss.NewStyle().
+		Height(common.ListPagerHeight).
+		MaxHeight(common.ListPagerHeight).
+		PaddingTop(1).
+		Foreground(theme.FaintText)
+
+	s.Table.CellStyle = lipgloss.NewStyle().PaddingLeft(1).
+		PaddingRight(1).
+		MaxHeight(1)
+	s.Table.SelectedCellStyle = s.Table.CellStyle.Copy().
+		Background(theme.SelectedBackground)
+	s.Table.TitleCellStyle = s.Table.CellStyle.Copy().
+		Bold(true).
+		Foreground(theme.PrimaryText)
+	s.Table.SingleRuneTitleCellStyle = s.Table.TitleCellStyle.Copy().Width(common.SingleRuneWidth)
+	s.Table.HeaderStyle = lipgloss.NewStyle().
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(theme.FaintBorder).
+		BorderBottom(true)
+	s.Table.RowStyle = lipgloss.NewStyle().
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(theme.FaintBorder).
+		BorderBottom(true)
+
+	return s
+}
